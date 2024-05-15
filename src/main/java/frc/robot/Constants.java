1a2880a4c8a68cfdcc3230ff147400215b55b8b8
Jade <spacey-sooty@proton.me>
2024-05-15 09:59:43 +0800

    Implement shooter subsystem (#1)
    To be added in later pr:
    - current or voltage limit
    - driver bindings

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public final class Constants {
+  public static final int driverport = 0;
+  public static final int shooterPort = 31;
+  public static final double shooterP = 0.02;
+  public static final double shooterI = 0.5;
+  public static final double shooterD = 0;
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,32 @@
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Shooter;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  private CommandXboxController m_driver;
+  private Shooter m_shooter;
 
-  private RobotContainer m_robotContainer;
+  private Command getAutonomousCommand() {
+    return null;
+  }
 
-  @Override
-  public void robotInit() {
-    m_robotContainer = new RobotContainer();
+  private void configureBindings() {}
+
+  @SuppressWarnings("removal")
+  public Robot() {
+    m_driver = new CommandXboxController(Constants.driverport);
+    m_shooter =
+        new Shooter(
+            new CANSparkMax(Constants.shooterPort, CANSparkMaxLowLevel.MotorType.kBrushless));
+
+    configureBindings();
   }
 
   @Override
@@ -30,7 +45,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
