9a67ce40b19929bf9d535cb52c97b2152f0f4402
Isaac Turner <spacey-sooty@proton.me>
2024-05-29 13:14:10 +0800

    [build] Add wpiformat

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 CurtinFRC
+// Open Source Software, you can modify it according to the terms
+// of the MIT License at the root of this project
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
