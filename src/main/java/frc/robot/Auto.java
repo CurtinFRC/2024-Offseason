4cb463606d35df05a346ca927de3de30a92de9a8
Jade <spacey-sooty@proton.me>
2024-06-07 11:08:55 +0800

    Update Auto.java

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -3,7 +3,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public interface Auto {
-  public void configureBindings();
+      public void configureBindings();
 
   public Command followTrajectory();
 }
