f6c5cc04de8ac249034afc886e2c386f17c45744
SturrockPeter <prs2016z@gmail.com>
2024-06-23 14:00:23 +0800

    changed LED states to be in intake and shooter subsystems

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,17 +11,21 @@ import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.PWM;
 
 public class Intake extends SubsystemBase {
   private CANSparkMax m_motor;
   private DataLog log = DataLogManager.getLog();
   private DoubleLogEntry log_output = new DoubleLogEntry(log, "/intake/output");
 
+  PWM lEDPwm = new PWM(0);
+
   public Intake(CANSparkMax motor) {
     m_motor = motor;
   }
 
   public Command intake() {
+    lEDPwm.setSpeed(0.59); // dark red //
     return Commands.run(
             () -> {
               log_output.append(4);
@@ -36,6 +40,7 @@ public class Intake extends SubsystemBase {
   }
 
   public Command outake() {
+    lEDPwm.setSpeed(0.63); // red orange, 0.65 for normal orange //
     return Commands.run(
             () -> {
               log_output.append(-4);
